dd6a04d2997f1907abd5ef9046c5bdb9f51afa15
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-03-05 23:36:11 +0300

    Solved coding challenge 3 of data structures and operators section

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -239,6 +239,31 @@ const gameEvents = new Map([
   [92, '🟡 Yellow card'],
 ]);
 
+// 1.
+const events = new Set();
+for (const [key, value] of gameEvents) {
+  events.add(value);
+}
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+
+// 3.
+console.log(
+  `An event happend, on average, every ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happend, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? `FIRST` : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
 /*
 /////////////////////
 Coding Challenge #4
